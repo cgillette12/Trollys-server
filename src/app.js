fdabe0c49da7dfc44700e510d80aa9446e579c80
Cody Gillette <cgillette1312@gmail.com>
2019-05-03 15:57:48 -0700

    updated

--- src/app.js
+++ src/app.js
@@ -4,7 +4,11 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const {NODE_ENV} = require('./config');
+const { NODE_ENV, API_TOKEN } = require('./config');
+const cardRouter = require('./card-router');
+const listRouter = require('./list-router');
+const logger = require('./logger');
+
 
 const app = express();
 
@@ -12,22 +16,29 @@ const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
+
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
-//Authoriazation
+// Authoriazation
 app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.apiToken;
+  const apiToken = API_TOKEN;
   const authToken = req.get('Authorization');
+  
+
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
     return res.status(401).json({ error: 'Unauthorized request' });
   }
   next();
 });
+app.use(cardRouter);
+app.use(listRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
